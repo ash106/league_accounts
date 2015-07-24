a4fe661ce84fc61de33c4ee33121fe143df2faec
Alex Howington <alex.howington@gmail.com>
2015-07-24 12:30:53 -0500

    Get solo queue stats for my accounts

--- Gemfile
+++ Gemfile
@@ -42,10 +42,12 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'dotenv-rails'
 end
 
 gem 'rails_12factor', group: :production
 gem 'puma'
 gem 'rack-timeout'
 gem 'bootstrap-sass', '~> 3.3.5'
+gem 'ruby-lol'
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,32 @@
+require 'lol'
+
 class StaticPagesController < ApplicationController
+  def home
+    @summoners = get_solo_queue_rank_for("na", "wheatbox", "bronziebox", "cheezebox", "plzdntkilme", "brotharhett")
+    @summoners << get_solo_queue_rank_for("lan", "hallonoops", "handsomemanna")
+    @summoners.flatten!
+  end
+
+  private
+
+    def get_solo_queue_rank_for(region, *names)
+      client = Lol::Client.new ENV["RIOT_API_KEY"], { region: region }
+      summoners = client.summoner.by_name(*names)
+      summoner_ids = []
+      summoners.each do |summoner|
+        summoner_ids << summoner.id
+      end
+      league_entries = client.league.get_entries(summoner_ids)
+      summoner_entries = []
+      summoners.each do |summoner|
+        league_entries["#{summoner.id}"].each do |entry|
+          if entry.queue == "RANKED_SOLO_5x5"
+            summoner_entries << { name: summoner.name, tier: entry.tier.capitalize,
+                                  division: entry.entries[0].division,
+                                  points: entry.entries[0].league_points }
+          end
+        end
+      end
+      summoner_entries
+    end
 end
